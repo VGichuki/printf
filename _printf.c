b35d6dd1ccc9c439d5ebb3b7666cb0a269a3b261
wangari <wangarigichukis@gmail.com>
2023-05-29 09:29:48 -0700

    changes

--- _printf.c
+++ _printf.c
@@ -1,66 +1,50 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - craetes printf
+ *
+ * @format: format specifier
+ *
+ * Return: Number of args length
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int (*get_f_spec)(va_list);
+	unsigned int index = 0, spec_count = 0;
+
+	va_list arg;
+
+	va_start(arg, format);
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (index = 0; format[index] != '\0'; index++)
 	{
-		if (format[i] != '%')
+		if (format[index] == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			index++;
+			if (format[index] == '\0')
+				return (-1);
+			while (format[index] == ' ')
+				index++;
+			get_f_spec = get_specifier(format[index]);
+
+			if (get_f_spec == NULL)
+			{
+				_putchar('%');
+				_putchar(format[index]);
+				spec_count += 2;
+			}
+			else
+				spec_count += get_f_spec(arg);
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			_putchar(format[index]);
+			spec_count++;
 		}
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	va_end(arg);
+	return (spec_count);
 }
